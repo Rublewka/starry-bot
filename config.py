312980913871c2ae51ec2dac664528acab90d136
TheSkout <111308237+TheSkout@users.noreply.github.com>
2023-02-28 16:53:38 +0300

    Bug fix

---
+++ config.py
@@ -0,0 +1,8 @@
+settings = {
+	'TOKEN': 'MTA3NDcyMTg5MDczNzg2NDcxNg.GtdbEY.hrbhDr3Hu1WHvABXHfH-FKkVJORJvt_yprqJ4E', # Получение токена - https://prnt.sc/uofhur
+	'NAME BOT': 'Rublewka BOT', # Имя вашего бота
+	'ID': '1074721890737864716', # Получение ID - https://prnt.sc/uofik2
+	'PREFIX': ';', # Префикс
+	'OWNER':'TheSkout#8213',    # ИЗМЕНЯЙТЕ НА СВОЙ NICK:ID (Пример: Toil#5605)
+	'OWNER NAME':'TheSkout' # ИМЯ ВЛАДЕЛЬЦА (Пример: Toil)
+}
