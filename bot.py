ae821282ecd60d0a9b78c412130b68bdaffe2e26
TheSkout <111308237+TheSkout@users.noreply.github.com>
2023-03-02 16:36:31 +0300

    bug fixes

--- bot.py
+++ bot.py
@@ -16,7 +16,6 @@ async def on_ready():
     await client.change_presence(status=discord.Status.dnd) #bot-status
 # Ping
 @client.command(aliases = ['Ping', 'PING', 'pING', 'ping'])
-#@client.command - объявление команды | (aliases = ['Ping', 'PING' ...]) - Альтернативное название команды
 async def __ping(ctx): # Объявление асинхронной функции __ping с возможностью публикации сообщения
     ping = client.ws.latency # Получаем пинг клиента
 
@@ -53,64 +52,58 @@ async def __help (ctx):
      title - Жирный крупный текст (Заголовок) | description - Текст под заголовком | colour - Цвет полоски
 
     emb.set_author(name = ctx.author.name, icon_url = ctx.author.avatar_url)
-    # Отображает: ctx.author.name - Имя отправителя, ctx.author.avatar_url - Аватар отправителя
-    emb.add_field( name = 'Информация', value = f'`{prefix}хелп` `{prefix}инфо` `{prefix}сервер` `{prefix}профиль` `{prefix}авторы` ', inline=False)
-    emb.add_field( name = 'Модерирование', value = f'`{prefix}мут` `{prefix}размут` `{prefix}бан` `{prefix}кик` `{prefix}очистить` ', inline=False)
-    # Отображаемый блок текста. name - Жирный крупный текст | value - обычный текст под "name" | inline = True - Блоки текста будут в одну строку (https://prnt.sc/uogw2x) / inline = False - Блоки текста будут один под другим (https://prnt.sc/uogx3t)
+    # Отображает:  Аватар отправителя
+    emb.add_field( name = 'Информация', value = f'`{prefix}help` `{prefix}ping` ', inline=False) # `{prefix}server` `{prefix}profile` 
+    # TODO - emb.add_field( name = 'Модерирование', value = f'`{prefix}mute` `{prefix}unmute` `{prefix}ban` `{prefix}kick` `{prefix}clear` ', inline=False)
     emb.set_thumbnail(url = client.user.avatar_url)
-     emb.set_thumbnail - Добавляет картинку около текста (Например: emb.set_thumbnail(url = "https://icons.iconarchive.com/icons/elegantthemes/beautiful-flat-one-color/128/unlocked-icon.png") (NOAD) добавит картинку замка (https://prnt.sc/uogztb)) | client.user.avatar_url - Отображает аватарку бота
-    emb.set_footer( icon_url = client.user.avatar_url, text = f'{client.user.name} © Copyright 2020 | Все права защищены' )
-     emb.set_thumbnail - Добавляет картинку под текстом | client.user.avatar_url - Аватарка бота | ctx.guild.name - Имя сервера
+    emb.set_footer( icon_url = client.user.avatar_url, text = f'{client.user.name} © Copyright 2023 | Все права защищены' )
 
     await ctx.send ( embed = emb)
-    # Отправляет сообщение и так же преобразует emb в embed
+    # преобразование embed 
 
     print(f'[Logs:info] Справка по командам была успешно выведена | {prefix}help ')
     # Информация, что команда "help" была использована
-    # Итог: https://prnt.sc/uoh6v6
 
-# Добавление аргументов асинхронной функции, реакции на сообщение:
 #Kick
-@client.command(aliases = ['Kick', 'kICK', 'KICK', 'kick'])
-@commands.has_permissions ( administrator = True ) # Команда только для пользователей имеющих роль с правами "Администратор"
-async def __kick(ctx, member: discord.Member, *, reason = None): # Асинхронная функция __kick(ctx, member: discord.Member, *, reason = None)
+#@client.command(aliases = ['Kick', 'kICK', 'KICK', 'kick'])
+#@commands.has_permissions ( administrator = True ) # Команда только для пользователей имеющих роль с правами "Администратор"
+#async def __kick(ctx, member: discord.Member, *, reason = None): # Асинхронная функция __kick(ctx, member: discord.Member, *, reason = None)
     #Аргументы: ctx - отправка сообщения с помощью команды (Обязательно)
     #Аргументы: member: discord.Member - "member" ----- может быть любой текст, но для удобства использую member (Discord.Member - для получения id указанного пользователя)
     #Аргументы: * - предыдущий аргумент необходим
     #Аргументы: reason = None - "reason" ----- может быть любой текст, но для удобства использовал reason, "None" - значение по умолчанию
-    await ctx.message.add_reaction('✅') # Добавляет реакцию к сообщению с командой
-    await member.kick( reason = reason ) # Кикнуть пользователя по причине (Преобразует причину бота в причину дискорда)
-    emb = discord.Embed( title = 'Kicked', description = f'Пользователь {member}  был кикнут по причине { reason } ', colour = discord.Color.red() )
-    emb.set_author( name = client.user.name )
-    emb.set_footer( text = ctx.author.name, icon_url = ctx.author.avatar_url )
-    emb.set_thumbnail(url = client.user.avatar_url)
+#    await ctx.message.add_reaction('✅') # Добавляет реакцию к сообщению с командой
+#    await member.kick( reason = reason ) # Кикнуть пользователя по причине (Преобразует причину бота в причину дискорда)
+#    emb = discord.Embed( title = 'Kicked', description = f'Пользователь {member}  был кикнут по причине { reason } ', colour = discord.Color.red() )
+#    emb.set_author( name = client.user.name )
+#    emb.set_footer( text = ctx.author.name, icon_url = ctx.author.avatar_url )
+#    emb.set_thumbnail(url = client.user.avatar_url)
 
-    await ctx.send( embed = emb )
+#    await ctx.send( embed = emb )
 
-    print(f'[Logs:moderation] Пользователь {member} был кикнут по причине {reason} | {prefix}kick ')
-    # Итог: https://prnt.sc/uohdqh
+#    print(f'[Logs:moderation] Пользователь {member} был кикнут по причине {reason} | {prefix}kick ')
 
-@__kick.error
-async def kick_error(ctx, goodbye):
-	if isinstance ( goodbye, commands.MissingRequiredArgument):
-		emb = discord.Embed( title = f'**Команда "{prefix}кик"**', description = f'Изгоняет указаного участника с сервера с возможностью возвращения ', colour = discord.Color.red() )
-		emb.set_author(name = ctx.author.name, icon_url = ctx.author.avatar_url)
-		emb.add_field( name = 'Использование', value = "!кик <@⁣Участник | ID>", inline=False)
-		emb.add_field( name = 'Пример', value = "`!кик @⁣Участник`\n┗ Кикнет указаного участника.", inline=False)
-		emb.set_thumbnail(url = client.user.avatar_url)
-		emb.set_footer( icon_url = client.user.avatar_url, text = f"{settings['OWNER NAME']} © Copyright 2020 | Все права защищены"   )
-		await ctx.send ( embed = emb)
-		print(f"[Logs:error] Необходимо указать участника | {prefix}kick")
 
-	if isinstance (goodbye, commands.MissingPermissions):
-		emb = discord.Embed( title = f'**Команда "{prefix}кик"**', description = f'Изгоняет указаного участника с сервера с возможностью возвращения ', colour = discord.Color.red() )
-		emb.set_author(name = ctx.author.name, icon_url = ctx.author.avatar_url)
-		emb.add_field( name = 'ОШИБКА!', value = "У вас недостаточно прав!", inline=False)
-		emb.set_thumbnail(url = client.user.avatar_url)
-		emb.set_footer( icon_url = client.user.avatar_url, text = f"{settings['OWNER NAME']} © Copyright 2020 | Все права защищены"   )
-		await ctx.send ( embed = emb)
-		print(f"[Logs:Error] [Ошибка доступа] Пользователь [{ctx.author}] попытался кикнуть | {prefix}kick")
+#@__kick.error
+#async def kick_error(ctx, goodbye):
+	#if isinstance ( goodbye, commands.MissingRequiredArgument):
+		#emb = discord.Embed( title = f'**Команда "{prefix}кик"**', description = f'Изгоняет указаного участника с сервера с возможностью возвращения ', colour = discord.Color.red() )
+		#emb.set_author(name = ctx.author.name, icon_url = ctx.author.avatar_url)
+		#emb.add_field( name = 'Использование', value = "!кик <@⁣Участник | ID>", inline=False)
+		#emb.add_field( name = 'Пример', value = "`!кик @⁣Участник`\n┗ Кикнет указаного участника.", inline=False)
+		#emb.set_thumbnail(url = client.user.avatar_url)
+		#emb.set_footer( icon_url = client.user.avatar_url, text = f"{settings['OWNER NAME']} © Copyright 2023 | Все права защищены"   )
+		#await ctx.send ( embed = emb)
+		#print(f"[Logs:error] Необходимо указать участника | {prefix}kick")
 
+	#if isinstance (goodbye, commands.MissingPermissions):
+		#emb = discord.Embed( title = f'**Команда "{prefix}кик"**', description = f'Изгоняет указаного участника с сервера с возможностью возвращения ', colour = discord.Color.red() )
+		#emb.set_author(name = ctx.author.name, icon_url = ctx.author.avatar_url)
+		#emb.add_field( name = 'ОШИБКА!', value = "У вас недостаточно прав!", inline=False)
+		#emb.set_thumbnail(url = client.user.avatar_url)
+		#emb.set_footer( icon_url = client.user.avatar_url, text = f"{settings['OWNER NAME']} © Copyright 2023 | Все права защищены"   )
+		#await ctx.send ( embed = emb)
+		#print(f"[Logs:Error] [Ошибка доступа] Пользователь [{ctx.author}] попытался кикнуть | {prefix}kick")
 
 
 
@@ -316,4 +309,5 @@ async def kick_error(ctx, goodbye):
 
 
 
-client.run (settings['TOKEN']) #Убираем в самый конец файла и больше не трогаем
+
+client.run (settings['TOKEN']) #DON'T TOUCH IT
