d6942a3d9d34169452e7037a58ee2c251a5e3335
TheSkout <daniil.afg@yandex.ru>
2023-03-04 13:15:42 +0300

    revert back

--- core.py
+++ core.py
@@ -12,7 +12,7 @@ client.remove_command('help')
 async def on_ready(): 
 
     print (f"Logged on as {settings['NAME BOT']}") # startup message in console
-    rbs = client.get_channel(channelsids['rublewka-bot-status'])
+    rbs = client.get_channel(1076240177032351765)
     await rbs.send(f'Successfull restart!') # startup message in status channel
     await client.change_presence(status=discord.Status.dnd) # presence
 
