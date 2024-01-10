047158d49580d5864ddd6ef94c6460747676e7f0
TheSkout <daniil.afg@yandex.ru>
2024-01-10 16:10:28 +0300

    0.23.1-APLHA

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ settings = {
 	'PREFIX': ';',                                                                                 # PREFIX
 	'OWNER':'@theskout001',                                                                        # OWNER USERNAME
 	'OWNER NAME':'TheSkout',                                                                       # OWNER NAME
-    'VERSION':'0.22.0-ALPHA'                                                                       # BOT VERSION
+    'VERSION':'0.23.1-ALPHA'                                                                       # BOT VERSION
 #			   ^ ^  ^
 #			   | |  | 
 #		   Major |  | 
